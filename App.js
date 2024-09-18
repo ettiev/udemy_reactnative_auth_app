b3f0a9ae0759f3464d646a293f32b67459fd497c
ettiev <126219891+ettiev@users.noreply.github.com>
2024-09-18 19:12:28 +0200

    Login and Signup functionality
    - Login functionality
    - Signup functionality
    - Reading info from Firebase database

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import { useContext, useEffect, useState, useCallback } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from "expo-splash-screen";
+  //npx expo install expo-splash-screen
 
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
+import AuthContextProvider, { AuthContext } from './store/auth-context';
+import IconButton from './components/ui/IconButton';
 
 const Stack = createNativeStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 function AuthStack() {
   return (
@@ -25,6 +32,7 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
   return (
     <Stack.Navigator
       screenOptions={{
@@ -33,25 +41,58 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen 
+        name="Welcome" 
+        component={WelcomeScreen} 
+        options={{
+          headerRight: ({ tintColor }) => <IconButton icon='exit' color={tintColor} size={24} onPress={authCtx.logout} />
+        }}
+      />
     </Stack.Navigator>
   );
 }
 
 function Navigation() {
+  const authCtx = useContext(AuthContext); 
   return (
     <NavigationContainer>
-      <AuthStack />
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
     </NavigationContainer>
   );
 }
 
+function Root() {
+  const authCtx = useContext(AuthContext);
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+  useEffect(() => {
+    async function fetchToken() {
+      //await SplashScreen.preventAutoHideAsync();
+      const storedToken = await AsyncStorage.getItem('token');
+      if (storedToken) {
+        authCtx.authenticate(storedToken);
+      }
+      setIsTryingLogin(false);
+      await SplashScreen.hideAsync();
+    }
+    fetchToken();    
+  }, []);
+  if (isTryingLogin) {
+    return null
+  }
+  return <Navigation />;
+}
+
 export default function App() {
+  
+
+
   return (
     <>
       <StatusBar style="light" />
-
-      <Navigation />
+      <AuthContextProvider>
+        <Root />      
+      </AuthContextProvider>
     </>
   );
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,35 @@
+import { useContext, useState } from 'react';
+
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticationg] = useState(false);
+  
+  const authCtx = useContext(AuthContext);
+
+  async function loginHandler({email, password}) {
+    setIsAuthenticationg(true);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);  
+    } catch (error) {
+      Alert.alert("Authentication failed!",
+        "Could not log you in! Check credentials or try again later."
+      )
+      setIsAuthenticationg(false);
+    }
+      
+  }
+  
+  if (isAuthenticating) {
+    return <LoadingOverlay message='Logging in...' />
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler}/>;
 }
 
 export default LoginScreen;
